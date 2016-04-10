415de2adf41649f8b9b3ed6919eeef99a8f7f514
Callebe Gomes <callebedrums@gmail.com>
2016-04-10 00:50:37 -0300

    finishing the configuration to run tests

--- karma.conf.js
+++ karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'public/app/**/*.js': ['babel', 'coverage']
+        'public/app/**/*.js': ['babel', 'sourcemap', 'coverage']
     },
 
     babelPreprocessor: {
